07e91a261cb9a3957d7224ee68bc3282771190dd
AmandaCrispim <amandacrispim@gmail.com>
2021-11-05 17:00:12 -0300

    firebase fruta

---
+++ firebase-javascript-amanda/js/init.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9Oemt_lfQOej3mjPg1JHOf5yOQIld05k",
+  authDomain: "lista-tarefas-amanda.firebaseapp.com",
+  projectId: "lista-tarefas-amanda",
+  storageBucket: "lista-tarefas-amanda.appspot.com",
+  messagingSenderId: "711312175048",
+  appId: "1:711312175048:web:683b1916fec67fb2b09afd"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
